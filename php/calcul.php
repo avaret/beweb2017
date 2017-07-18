4e5122bed0e1019a5e96ca968607af83fe94bb1b
Ness <masc67.hang@gmail.com>
2017-07-18 11:45:24 +0200

    Working on calculus Kidding, just debugging

---
+++ php/calcul.php
@@ -0,0 +1,58 @@
+<?php
+DEFINE("SERV","locahost");
+DEFINE("LOGIN","root");
+DEFINE("MDP","");
+DEFINE("NOM_BD","beweb_2017");
+
+class Airport
+{
+    public $code;
+    public $lat;
+    public $long;
+    public $zone; //7 is corse, 0 no zone
+    public function __construct($code, $lat, $long, $zone) 
+    {
+              $this->code = $code;
+              $this->lat = $lat;
+              $this->long = $long;
+              $this->zone = $zone;
+    }
+}
+
+
+function connexion()
+{
+    try
+    {
+        $connStr="mysql:host=".SERV.";dbname=".NOM_BD;
+        $dbh=new PDO($connStr, LOGIN, MDP);
+    }
+    catch(PDOException $e)
+    {
+        echo 'Connection failed: '.$e->getMessage();
+        return "fail";
+    }
+    return $dbh;
+}
+
+function getAirport()
+{
+    $dbh=connexion();
+    $sql="SELECT codeOACI, lon, lat, no_zone FROM aerodrome;";
+    $sth=$dbh->prepare($sql);
+    $sth->execute();
+    $listA=array();
+    $id=0;
+    $html="<br\>";
+    while($result=$sth->fetch(PDO::FETCH_OBJ))
+    {
+        $listA[$id] = new Airport($result->codeOACI,$result->lon,$result->lat, $result->no_zone);
+        //$html.=$listA[$id]
+        $id++;
+    }
+    $sth->closeCursor();
+    /*FOR TEST*/ print_r($listA); /*FOR TEST*/
+    return $listA;
+}
+
+?>
