546a3ea63da401ac855e4181e552e94b015dae75
Ness <masc67.hang@gmail.com>
2017-07-20 13:44:35 +0200

    Conflict

--- js/map.php
+++ js/map.php
@@ -169,28 +169,28 @@ function initMap() {
       strokeWeight: 2
     });
     
-    var departure = new google.maps.LatLng(dept_lat, dept_lng); //Set to whatever lat/lng you need for your departure location
-    var arrival = new google.maps.LatLng(arr_lat, arr_lng); //Set to whatever lat/lng you need for your arrival location
-    var line = new google.maps.Polyline({
-        path: [departure, departure],
-        strokeColor: "#FF0000",
-        strokeOpacity: 1,
-        strokeWeight: 1,
-        geodesic: true, //set to false if you want straight line instead of arc
-        map: map,
-    });
-    var step = 0;
-    var numSteps = 250; //Change this to set animation resolution
-    var timePerStep = 5; //Change this to alter animation speed
-    var interval = setInterval(function() {
-    step += 1;
-    if (step > numSteps) {
-        clearInterval(interval);
-    } else {
-        var are_we_there_yet = google.maps.geometry.spherical.interpolate(departure,arrival,step/numSteps);
-        line.setPath([departure, are_we_there_yet]);
-    }
-    }, timePerStep);
+//    var departure = new google.maps.LatLng(dept_lat, dept_lng); //Set to whatever lat/lng you need for your departure location
+//    var arrival = new google.maps.LatLng(arr_lat, arr_lng); //Set to whatever lat/lng you need for your arrival location
+//    var line = new google.maps.Polyline({
+//        path: [departure, departure],
+//        strokeColor: "#FF0000",
+//        strokeOpacity: 1,
+//        strokeWeight: 1,
+//        geodesic: true, //set to false if you want straight line instead of arc
+//        map: map,
+//    });
+//    var step = 0;
+//    var numSteps = 250; //Change this to set animation resolution
+//    var timePerStep = 5; //Change this to alter animation speed
+//    var interval = setInterval(function() {
+//    step += 1;
+//    if (step > numSteps) {
+//        clearInterval(interval);
+//    } else {
+//        var are_we_there_yet = google.maps.geometry.spherical.interpolate(departure,arrival,step/numSteps);
+//        line.setPath([departure, are_we_there_yet]);
+//    }
+//    }, timePerStep);
 
     flightPath.setMap(map);
     
