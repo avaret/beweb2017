ed00d92fc5c7d22533c8b71f72cde5776bc41f95
cendr <mathieu.rolland.isesa@gmail.com>
2017-07-24 16:16:43 +0200

    indentation proprifiée!

--- php/template.php
+++ php/template.php
@@ -6,7 +6,7 @@ session_start();
 function entete($title1, $forMap = false, $forreg = false)
 {
 
-	$entete1='
+    $entete1='
 <!DOCTYPE html>
 <html lang="fr"><head>
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8"><title>'.$title1.'</title>
@@ -19,11 +19,11 @@ function entete($title1, $forMap = false, $forreg = false)
 		<link rel="stylesheet" href="/beweb2017/library/font-Montserrat.css">
 		<link rel="stylesheet" href="/beweb2017/library/font-awesome.css">
 		<link rel="stylesheet" href="/beweb2017/css/style.css">';
-	if($forreg){
-		$entete1.=' 
+    if($forreg){
+        $entete1.=' 
 			<link rel="stylesheet" href="/beweb2017/css/register.css">';
-	}
-	$entete1.='
+    }
+    $entete1.='
 		<link rel="stylesheet" href="/beweb2017/library/bootstrap.min.css">
 		<link rel="stylesheet" href="/beweb2017/library/bootstrap-table.min.css">
 	<style>
@@ -31,8 +31,8 @@ function entete($title1, $forMap = false, $forreg = false)
 		.w3-bar,h1,button {font-family: "Montserrat", sans-serif}
 		.fa-anchor,.fa-coffee {font-size:200px}';
 
-	if($forMap) {
-		$entete1.='
+    if($forMap) {
+        $entete1.='
 		#map {
 		    height: 100%;
 		}
@@ -68,87 +68,87 @@ function entete($title1, $forMap = false, $forreg = false)
 	    position: absolute;
 	    left: 30px;
 	    }';
-	}
+    }
 
-	$entete1.="\n	</style>\n</head>\n<body> ";
-	return $entete1;
+    $entete1.="\n	</style>\n</head>\n<body> ";
+    return $entete1;
 }
 
 function navbar($isRegistering = false){
-	$pages = array( 
+    $pages = array( 
         array( "index.php",
-               "avion.ico",
-               "Accueil"
+              "avion.ico",
+              "Accueil"
              ),
         array( "php/rules.php",
-               "sifflet.png",
-               "Les règles"
+              "sifflet.png",
+              "Les règles"
              ),
         array( "php/addflight.php",
-               "drapeau.png",
-               "Ajouter un vol"
+              "drapeau.png",
+              "Ajouter un vol"
              ),
         array( "php/map.php",
-               "map.png",
-               "Map"
+              "map.png",
+              "Map"
              ),
         array( "php/scores.php",
-               "podium.png",
-               "Scores"
+              "podium.png",
+              "Scores"
              )	
     );
 
-	$navbar1=" \n\n <!--- NAVBAR  --> \n";
+    $navbar1=" \n\n <!--- NAVBAR  --> \n";
 
-	if(isset($_GET["err"]))
-	{$navbar1 .= '<script>window.alert("Echec d\'authentification")</script>';}
+    if(isset($_GET["err"]))
+    {$navbar1 .= '<script>window.alert("Echec d\'authentification")</script>';}
 
-	if(isset($_GET["logout"]))
-	{
-		$navbar1 .= '<script>window.alert("Merci de votre visite!")</script>';
-		session_unset();
-		session_destroy();
-	}
+    if(isset($_GET["logout"]))
+    {
+        $navbar1 .= '<script>window.alert("Merci de votre visite!")</script>';
+        session_unset();
+        session_destroy();
+    }
 
-	$navbar1 .=   '
+    $navbar1 .=   '
 <div class="w3-top">
   <div class="w3-bar w3-blue w3-card-2 w3-large">
     <span class="w3-left-align">
     <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-blue" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>';
 
-	for($row = 0; $row < 5; $row++)
-	{
-		$navbar1 .= "\n".'    <a href="http://localhost/beweb2017/' . $pages[$row][0] . '" class="w3-bar-item w3-button w3-hide-small w3-hover-white w3-border-right"><img src="/beweb2017/images/'.$pages[$row][1].'" width="16" alt="icone"> ' . $pages[$row][2] . '</a>';
-	}
+    for($row = 0; $row < 5; $row++)
+    {
+        $navbar1 .= "\n".'    <a href="http://localhost/beweb2017/' . $pages[$row][0] . '" class="w3-bar-item w3-button w3-hide-small w3-hover-white w3-border-right"><img src="/beweb2017/images/'.$pages[$row][1].'" width="16" alt="icone"> ' . $pages[$row][2] . '</a>';
+    }
 
 
-	if(isset($_SESSION["admin"]))
-	{
-		if($_SESSION["admin"]==1)
-		{
-			$navbar1 .= '<a href="http://localhost/beweb2017/php/pageadmin.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white w3-border-right"><img src="/beweb2017/images/admin.png" width="16" alt="icone"> Administration</a>  ';
-		}
-	}
-	$navbar1 .= '
+    if(isset($_SESSION["admin"]))
+    {
+        if($_SESSION["admin"]==1)
+        {
+            $navbar1 .= '<a href="http://localhost/beweb2017/php/pageadmin.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white w3-border-right"><img src="/beweb2017/images/admin.png" width="16" alt="icone"> Administration</a>  ';
+        }
+    }
+    $navbar1 .= '
     <a href="http://localhost/beweb2017/php/contacts.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white"><img src="/beweb2017/images/phone.png" width="16" alt="icone"> Contacts</a>
     </span>
     <span class="w3-right-align" style="float:right">
 
     <div id="navthing">';
 
-	if(isset($_SESSION["login"]))
-	{
-		$navbar1 .= ''.$_SESSION["login"].'   <a href="/beweb2017/index.php?logout" class="w3-bar-item w3-button w3-hide-small  w3-hover-white">Se déconnecter</a>     ';
-	}
-	else
-	{
-		$navbar1 .= '
+    if(isset($_SESSION["login"]))
+    {
+        $navbar1 .= ''.$_SESSION["login"].'   <a href="/beweb2017/index.php?logout" class="w3-bar-item w3-button w3-hide-small  w3-hover-white">Se déconnecter</a>     ';
+    }
+    else
+    {
+        $navbar1 .= '
 	    <a href="#" id="loginform" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Login</a>';
 
-	if(!$isRegistering) 
-	{	
-	    $navbar1 .= '  <a href="http://localhost/beweb2017/php/register.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white w3-border-left">Register</a>     ';
-	}
+        if(!$isRegistering) 
+        {	
+            $navbar1 .= '  <a href="http://localhost/beweb2017/php/register.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white w3-border-left">Register</a>     ';
+        }
 
 
     }
@@ -161,19 +161,19 @@ function navbar($isRegistering = false){
 
   <!-- Navbar on small screens -->
   <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">';
-    
+
     for($row = 0; $row < 5; $row++)
-	{
-		$navbar1 .= "\n".'    <a href="http://localhost/beweb2017/' . $pages[$row][0] . '" class="w3-bar-item w3-button w3-padding-large">' . $pages[$row][2] . '</a>';
-	}
+    {
+        $navbar1 .= "\n".'    <a href="http://localhost/beweb2017/' . $pages[$row][0] . '" class="w3-bar-item w3-button w3-padding-large">' . $pages[$row][2] . '</a>';
+    }
 
 
     if(isset($_SESSION["admin"]))
     {
-	    if($_SESSION["admin"]==1)
-	    {
-		    $navbar1 .= '<a href="http://localhost/beweb2017/php/pageadmin.php" class="w3-bar-item w3-button w3-padding-large">PageAdmin</a>  ';
-	    }
+        if($_SESSION["admin"]==1)
+        {
+            $navbar1 .= '<a href="http://localhost/beweb2017/php/pageadmin.php" class="w3-bar-item w3-button w3-padding-large">PageAdmin</a>  ';
+        }
     }
     $navbar1 .= '
     <a href="http://localhost/beweb2017/php/contacts.php" class="w3-bar-item w3-button w3-padding-large">Contacts</a>
@@ -181,7 +181,7 @@ function navbar($isRegistering = false){
      ';
     if(!$isRegistering) 
     {
-	    $navbar1 .= '<a href="http://localhost/beweb2017/php/register.php" class="w3-bar-item w3-button w3-padding-large">Register</a>';
+        $navbar1 .= '<a href="http://localhost/beweb2017/php/register.php" class="w3-bar-item w3-button w3-padding-large">Register</a>';
     }
     $navbar1 .= '
   </div>
@@ -210,7 +210,7 @@ function navbar($isRegistering = false){
 }
 
 function footer(){
-	$footer1='
+    $footer1='
 <footer class="w3-container w3-padding-64 w3-center w3-opacity">  
  <p>Un projet pour l\'<a href="http://www.enac.fr/" target="_blank"><u>ENAC</u></a> | <a href="http://localhost/beweb2017/php/contacts.php"target="_blank"> <u>Nous contacter</u></p>
 </footer>
@@ -237,7 +237,7 @@ function myFunction() {
 
 </body></html>
 ';
-	return $footer1;
+    return $footer1;
 }
 
 ?>
